1c716d30967464d117352f81379dc956a08cadf8
Mark E. DeYoung <mark.e.deyoung@gmail.com>
2020-09-13 15:57:29 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ cat /srv/ubuntu_config/mlx4_modules >> /etc/modules
 # Install and configure avahi (AKA Bonjour)
 apt -y install avahi-daemon avahi-utils
 systemctl enable avahi-daemon
-avahi-set-host-name $(hostname)
+sed --in-place=.orig "s/#host-name=foo/host-name=$(hostname)/" /etc/avahi/avahi-daemon.conf
 
 # Update and Upgrade
 apt -y update
