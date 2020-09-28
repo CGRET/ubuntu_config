92f88221f26e8e6fe39eaf6e035299e17db61ac2
Mark E. DeYoung <mark.e.deyoung@gmail.com>
2020-09-28 03:18:24 +0300

    Update 01-infiniband.sh

--- bootstrap.d/01-infiniband.sh
+++ bootstrap.d/01-infiniband.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+# Install Mellanox Firware Tools (MFT)
+apt --yes install build-essential make dkms wget
+wget https://www.mellanox.com/downloads/MFT/mft-4.15.0-104-x86_64-deb.tgz
+tar xvzf mft-4.15.0-104-x86_64-deb.tgz
+pushd .
+cd mft-4.15.0-104-x86_64-deb
+$(pwd)/install.sh
+popd
+
+
 # Install InfiniBand drivers and software
-apt -y install opensm ibutils ibverbs-utils rdmacm-utils libibmad-dev numactl perftest iperf qperf mstflint
+apt --yes install opensm ibutils ibverbs-utils rdmacm-utils libibmad-dev numactl perftest iperf qperf mstflint
 systemctl enable opensm
 
 cat /srv/ubuntu_config/mlx4_modules >> /etc/modules
