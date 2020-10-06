cf92547345be2f30eb80b4a1bdf8f3977b4bff5f
Owner <owner@maas-001>
2020-10-06 15:56:35 +0000

    Added ssh.service for avahi

--- bootstrap.d/02-avahi.sh
+++ bootstrap.d/02-avahi.sh
@@ -5,3 +5,5 @@ apt -y install avahi-daemon avahi-utils
 systemctl enable avahi-daemon
 sed --in-place=.orig "s/#host-name=foo/host-name=$(hostname)/" /etc/avahi/avahi-daemon.conf
 
+cp /srv/ubuntu_config/etc/avahi/services/ssh.service /etc/avahi/services/ssh.service
+
