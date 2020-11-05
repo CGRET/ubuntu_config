e33619d7a3927f4bfb0403cd25cd901c4e3f653e
Owner <owner@maas-003.maas>
2020-11-05 21:08:14 +0000

    Update avahi domain name.

--- bootstrap.d/02-avahi.sh
+++ bootstrap.d/02-avahi.sh
@@ -3,7 +3,11 @@
 # Install and configure avahi (AKA Bonjour)
 apt -y install avahi-daemon avahi-utils
 systemctl enable avahi-daemon
-sed --in-place=.orig "s/#host-name=foo/host-name=$(hostname)/" /etc/avahi/avahi-daemon.conf
+
+# Update hostname
+sed --in-place "s/#host-name=foo/host-name=$(hostname)/" /etc/avahi/avahi-daemon.conf
+# Upaate domain name
+sed --in-place "s/#domain-name=local/domain-name=maas/" /etc/avahi/avahi-daemon.conf
 
 cp /srv/ubuntu_config/etc/avahi/services/ssh.service /etc/avahi/services/ssh.service
 
