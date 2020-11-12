a011e4f1a7d6fa1f899de80947c4917fa6b74e50
Owner <owner@maas-003.maas>
2020-11-12 13:00:48 +0000

    Add ldap utilities

---
+++ bootstrap.d/05-ldap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Install ldap utilities
+apt-get install --yes ldap-utils
+
+# Install Active Directory CA certificate
+cp /srv/ubuntu_config/usr/lib/ssl/certs/cdn.local.pem /usr/lib/ssl/certs/cdn.local.pem
+c_rehash
+
+# Configure ldap utilities
+mkdir -p /etc/ldap
+cat /srv/ubuntu_config/etc/hosts >> /etc/hosts
+cat /srv/ubuntu_config/etc/ldap/ldap.conf >> /etc/ldap.conf
+
