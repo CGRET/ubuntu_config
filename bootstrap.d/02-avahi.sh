23246b8fd3112621e1ddbc8e3a83cdfbb8df4ac4
Owner <owner@maas-003.maas>
2020-11-19 19:33:58 +0000

    Turn off avahi.

--- bootstrap.d/02-avahi.sh
+++ bootstrap.d/02-avahi.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Install and configure avahi (AKA Bonjour)
-apt -y install avahi-daemon avahi-utils
-systemctl enable avahi-daemon
+#apt -y install avahi-daemon avahi-utils
+#systemctl enable avahi-daemon
 
 # Update hostname
-sed --in-place "s/#host-name=foo/host-name=$(hostname)/" /etc/avahi/avahi-daemon.conf
+#sed --in-place "s/#host-name=foo/host-name=$(hostname)/" /etc/avahi/avahi-daemon.conf
 # Upaate domain name
-sed --in-place "s/#domain-name=local/domain-name=maas/" /etc/avahi/avahi-daemon.conf
+#sed --in-place "s/#domain-name=local/domain-name=maas/" /etc/avahi/avahi-daemon.conf
 
-cp /srv/ubuntu_config/etc/avahi/services/ssh.service /etc/avahi/services/ssh.service
+#cp /srv/ubuntu_config/etc/avahi/services/ssh.service /etc/avahi/services/ssh.service
 
