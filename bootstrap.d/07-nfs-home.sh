7638714747fa5be2b44ce581f9ddf587c6f97d55
Owner <owner@maas-003.maas>
2020-12-08 14:12:14 +0000

    NFS config change.

--- bootstrap.d/07-nfs-home.sh
+++ bootstrap.d/07-nfs-home.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 apt-get --yes install  nfs-common
-echo "192.168.238.23:/export/home   /home   nfs    auto  0  0" >> /etc/fstab
+mkdir /share
+echo "192.168.238.23:/export/   /share   nfs    auto  0  0" >> /etc/fstab
