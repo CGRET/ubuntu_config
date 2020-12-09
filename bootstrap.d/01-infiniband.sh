cec7e1a33de0310b6e28edfafea601de5b7f416d
Owner <owner@maas-003.maas>
2020-12-09 21:34:31 +0000

    Infiniband troubleshooting.

--- bootstrap.d/01-infiniband.sh
+++ bootstrap.d/01-infiniband.sh
@@ -18,5 +18,16 @@ cat /srv/ubuntu_config/etc/mlx4_modules >> /etc/modules
 
 cp /srv/ubuntu_config/etc/modprobe.d/mlx4_core.conf /etc/modprobe.d/mlx4_core.conf
 
+# Activate the ipoib interface
+modprobe mlx4_core
+modprobe mlx4_ib
+modprobe ib_ipoib
+modprobe ib_umad
+modprobe rdma_ucm
+
+#Get the name of the ipoib interface
+IPoIB=$(ip --brief link show type ipoib | cut -d' ' -f1)
+
 cp /srv/ubuntu_config/etc/netplan/60-infiniband.yaml /etc/netplan/60-infiniband.yaml
+echo "#${IPoIB}" >> /etc/netplan/60-infiniband.yaml
 
