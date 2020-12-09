e518129b67bd79e534a300c310fb7a3203edd264
Owner <owner@maas-003.maas>
2020-12-09 16:02:10 +0000

    NFS work

--- bootstrap.d/07-nfs.sh
+++ bootstrap.d/07-nfs.sh
@@ -3,5 +3,12 @@
 apt-get --yes install  nfs-common
 mkdir /share
 chmod 777 /share
-echo "#10.236.1.23:/export/share	/share	nfs	auto	0	0" >> /etc/fstab
-echo "192.168.238.23:/export/share	/share	nfs	auto	0	0" >> /etc/fstab
+# Count ConnectX cards
+CX=$(lspci | grep ConnectX | wc -l)
+if [ "$CX" -eq "0" ]; then
+	echo "#10.236.1.23:/export/share        /share  nfs     auto    0       0" >> /etc/fstab
+	echo "192.168.238.23:/export/share      /share  nfs     auto    0       0" >> /etc/fstab
+        exit
+fi
+echo "10.236.1.23:/export/share	/share	nfs	auto	0	0" >> /etc/fstab
+echo "#192.168.238.23:/export/share	/share	nfs	auto	0	0" >> /etc/fstab
