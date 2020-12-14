bcf1bf51abb7781d65b91fcd5c4d6aba8cc93d9a
root <root@maas-003.maas.dss.cdn.local>
2020-12-14 18:55:50 +0000

    Add note.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # This scripts is called by curtin.
 # Check /etc/maas/preseeds/curtin_userdata for the invocation
-
+# /snap/maas/current/etc/maas/preseeds/ if you installed maas using snap
 
 for f in /srv/ubuntu_config/bootstrap.d/*.sh; do
 	bash "$f"
