eaec2f796547f7e1ea685cce1f7ff35841100159
Owner <owner@maas-003.maas>
2020-12-14 19:27:35 +0000

    Update.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@
 
 # This scripts is called by curtin.
 # Check /etc/maas/preseeds/curtin_userdata for the invocation
-# /snap/maas/current/etc/maas/preseeds/ if you installed maas using snap
+# /var/snap/maas/current/preseeds/ if you installed maas using snap
+# See: https://maas.io/docs/snap/2.8/ui/custom-machine-setup
 
 for f in /srv/ubuntu_config/bootstrap.d/*.sh; do
 	bash "$f"
