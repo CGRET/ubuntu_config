0b25b685741b4d3e0af0cc24ca540015068502d6
Owner <owner@maas-003.maas>
2020-12-22 22:50:01 +0000

    Basic settings for slurm and munge.

---
+++ bootstrap.d/09-slurm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Create users and groups for slurm and munge
+adduser -u 1111 munge --disabled-password --gecos ""
+adduser -u 1121 slurm --disabled-password --gecos ""
+
+addgroup -gid 1111 munge
+addgroup -gid 1121 slurm
+adduser -u 1111 munge --disabled-password --gecos "" -gid 1111
+adduser -u 1121 slurm --disabled-password --gecos "" -gid 1121
+
