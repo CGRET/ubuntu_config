7aa4e76998d297300f8184c8c534935fc7aa8009
Owner <owner@maas-003.maas>
2020-12-28 20:09:49 +0000

    Working on sigularity install.

--- bootstrap.d/10-singularity.sh
+++ bootstrap.d/10-singularity.sh
@@ -5,18 +5,13 @@ sudo apt-get update \
 && sudo apt-get install -y build-essential libseccomp-dev pkg-config squashfs-tools cryptsetup sudo
 
 
-# Download go
-export VERSION=1.14.12 OS=linux ARCH=amd64 wget -O /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz
-https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz \ && sudo tar -C /usr/local -xzf
-/tmp/go${VERSION}.${OS}-${ARCH}.tar.gz
+# Download and extract go into /usr/local
+export VERSION=1.14.12 OS=linux ARCH=amd64 \
+&& wget -O /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz \
+&& sudo tar -C /usr/local -xzf /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz
 
 # Configure go
-echo 'export GOPATH=${HOME}/go' >> ~/.bashrc \
-&& echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc \
-&& source ~/.bashrc
-
-# curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin v1.21.0
-
+export PATH=/usr/local/go/bin:${PATH}
 
 # Download, build, and install singularity
 
@@ -24,10 +19,11 @@ pushd .
 mkdir singularity
 cd singularity
 
-wget https://github.com/hpcng/singularity/releases/download/v3.7.0/singularity-3.7.0.tar.gz
-tar xzf singularity-3.7.0.tar.gz
+export VERSION=3.7.0 && # adjust this as necessary \
+wget https://github.com/hpcng/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz \
+&& tar -xzf singularity-${VERSION}.tar.gz \
+&&  cd singularity
 
-cd singularity/
 $(pwd)/mconfig \
 &&   cd ./builddir \
 &&   make \
