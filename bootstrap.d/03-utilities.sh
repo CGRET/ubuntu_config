a1a119ccaba7d47788d956694a2c479a435f4567
Owner <owner@maas-003.maas>
2020-12-29 13:51:52 +0000

    Install maas-cli

--- bootstrap.d/03-utilities.sh
+++ bootstrap.d/03-utilities.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #apt --yes install environment-modules
-apt --yes install ubuntu-drivers-common inetutils-traceroute
+apt --yes install ubuntu-drivers-common inetutils-traceroute maas-cli
 
 # Install golang for use by singularity
 snap install go --classic
