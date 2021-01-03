62289c64b14273b385d1f79e0ba334588d7ed091
Owner <owner@maas-003.maas>
2021-01-03 12:30:46 +0000

    Prep for re-deployment.

--- bootstrap.d/08-ldap.sh
+++ bootstrap.d/08-ldap.sh
@@ -3,15 +3,3 @@
 # Install ldap utilities
 apt-get install --yes ldap-utils
 
-# Install Active Directory CA certificate
-cp /srv/ubuntu_config/usr/lib/ssl/certs/cdn.local.pem /usr/lib/ssl/certs/cdn.local.pem
-c_rehash
-
-# Configure ldap utilities
-mkdir -p /etc/ldap
-
-# This is overwritten by MAAS, might be able to fix with systemd-resolve
-cat /srv/ubuntu_config/etc/hosts >> /etc/hosts
-
-cat /srv/ubuntu_config/etc/ldap/ldap.conf >> /etc/ldap/ldap.conf
-
