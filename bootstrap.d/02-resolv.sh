7c1a040dedd588f8441e7c28a567bd4424ce52aa
Owner <owner@ipa.css.cdn.local>
2021-01-08 13:15:02 +0000

    Update service locations.

--- bootstrap.d/02-resolv.sh
+++ bootstrap.d/02-resolv.sh
@@ -2,7 +2,7 @@
 
 
 # Update DNS
-sed --in-place "s/#DNS=/DNS=10.236.0.22/" /etc/systemd/resolved.conf
+sed --in-place "s/#DNS=/DNS=10.236.0.23/" /etc/systemd/resolved.conf
 
 # Update Domains
 sed --in-place "s/#Domains=/Domains=dss.cdn.local/" /etc/systemd/resolved.conf
--- bootstrap.d/05-nfs.sh
+++ bootstrap.d/05-nfs.sh
@@ -6,9 +6,9 @@ chmod 777 /share
 # Count ConnectX cards
 CX=$(lspci | grep ConnectX | wc -l)
 if [ "$CX" -eq "0" ]; then
-	echo "10.236.1.22:/exports/share        /share  nfs     auto    0       0" >> /etc/fstab
-	echo "#192.168.238.22:/exports/share      /share  nfs     auto    0       0" >> /etc/fstab
+	echo "10.236.0.23:/exports/share        /share  nfs     auto    0       0" >> /etc/fstab
+	echo "#192.168.238.23:/exports/share      /share  nfs     auto    0       0" >> /etc/fstab
         exit 0
 fi
-echo "#10.236.1.22:/exports/share	/share	nfs	auto	0	0" >> /etc/fstab
-echo "192.168.238.22:/exports/share	/share	nfs	auto	0	0" >> /etc/fstab
+echo "#10.236.0.23:/exports/share	/share	nfs	auto	0	0" >> /etc/fstab
+echo "192.168.238.23:/exports/share	/share	nfs	auto	0	0" >> /etc/fstab
