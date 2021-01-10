7d1ccf7e6afa926d1bae701d1f665afc2535f74f
Owner <owner@ipa.css.cdn.local>
2021-01-10 08:27:41 -0500

    Add cockpit & cockpit-machines

--- bootstrap.d/03-utilities.sh
+++ bootstrap.d/03-utilities.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #apt --yes install environment-modules
-apt --yes install ubuntu-drivers-common inetutils-traceroute golang
+apt-get --yes install ubuntu-drivers-common inetutils-traceroute golang cockpit cockpit-machines
 
 
