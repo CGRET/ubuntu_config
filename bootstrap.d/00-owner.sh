fb2bace512f191f56a7341f3b07f6593d989de23
Owner <owner@ipa.css.cdn.local>
2021-01-10 19:42:07 -0500

    Modified owner setup.

--- bootstrap.d/00-owner.sh
+++ bootstrap.d/00-owner.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 
-echo "Creating owner"
+USER="owner"
+echo "Creating ${USER}"
 
 if [ ! -f /srv/.pass ]; then
-        echo "Missing password.  Will not create owner"
+        echo "Missing password.  Will not create ${USER}"
         exit 0
 fi
 
 PASSWORD=$(cat /srv/.pass)
+
+adduser ${USER}
+
+mkdir /${USER}
+chown ${USER}:${USER} /${USER}
+chmod 700 /${USER}
+usermod --home /${USER} ${USER}
+cp -a /home/${USER} /${USER}
+echo ${PASSWORD} | passwd ${USER}
+
+# use sudo or wheel
+GROUP=$(getent group sudo | cut -d':' -f1)
+GROUP=${GROUP:-$(getent group wheel | cut -d':' -f1)}
+
+# If the usermod succeeds then
+# create a sudoers.d file for the user.
+if usermod -aG ${GROUP} ${USER}; then
+  FILE="/etc/sudoers.d/${USER}"
+  echo "${USER} ALL=(ALL) NOPASSWD: ALL" > ${FILE}
+fi
 
 adduser --create-home --home /owner --password ${PASSWORD}
 
