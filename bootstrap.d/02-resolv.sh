46be4d0a77dfce353fbab6247ace7abcab63a9f6
Owner <owner@ipa.css.cdn.local>
2021-01-12 13:41:46 -0500

    Turn resolved.conf changes back on.

--- bootstrap.d/02-resolv.sh
+++ bootstrap.d/02-resolv.sh
@@ -3,11 +3,11 @@
 # Set the DNS server in the subnet on MAAS
 #  probably don't need to do the global config change anymore.
 
-#echo "Modifying systemd-resolved configuration."
-#echo "Update DNS server"
-#sed --in-place "s/#DNS=/DNS=10.236.0.23/" /etc/systemd/resolved.conf
+echo "Modifying systemd-resolved configuration."
+echo "Update DNS server"
+sed --in-place "s/#DNS=/DNS=10.236.0.23/" /etc/systemd/resolved.conf
 
-#echo "Update Domains"
-#sed --in-place "s/#Domains=/Domains=dss.cdn.local/" /etc/systemd/resolved.conf
+echo "Update Domains"
+sed --in-place "s/#Domains=/Domains=dss.cdn.local/" /etc/systemd/resolved.conf
 
 
