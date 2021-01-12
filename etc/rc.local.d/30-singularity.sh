78120d21c2c96d9fce529b6666df27230e9c61b2
Owner <owner@ipa.css.cdn.local>
2021-01-12 16:53:46 -0500

    Moved signularity build to rc.local.d one shot.

--- etc/rc.local.d/30-singularity.sh
+++ etc/rc.local.d/30-singularity.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+INSTALLED=$(which singularity)
+if [ ! "$INSTALLED" -eq "0" ]; then
+
 echo "Install singularity pre-reqs."
 apt-get --yes --quiet install build-essential libseccomp-dev pkg-config squashfs-tools cryptsetup
 
@@ -7,8 +10,8 @@ apt-get --yes --quiet install build-essential libseccomp-dev pkg-config squashfs
 echo "Download, build, and install singularity."
 
 pushd .
-mkdir singularity
-cd singularity
+BUILDDIR=$(mktemp -d)
+cd ${BUILDDIR}
 
 export VERSION=3.7.0 && # adjust this as necessary \
 wget --quiet https://github.com/hpcng/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz \
@@ -22,3 +25,5 @@ $(pwd)/mconfig \
 
 popd
 
+
+fi
