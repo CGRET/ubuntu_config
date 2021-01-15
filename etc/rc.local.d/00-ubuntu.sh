357a482febf9f9ce5f47d870a097b5c9654df57a
Owner <owner@dss.cdn.local>
2021-01-15 20:03:38 +0000

    One shoot change ubuntu users home.

---
+++ etc/rc.local.d/00-ubuntu.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+USER="ubuntu"
+echo "Creating ${USER}"
+
+if [ ! -f /srv/.pass ]; then
+        echo "Missing password.  Will not update ${USER}"
+        exit 0
+fi
+
+PASSWORD=$(cat /srv/.pass)
+
+
+mkdir /${USER}
+chown ${USER}:${USER} /${USER}
+chmod 700 /${USER}
+usermod --home /${USER} ${USER}
+cp -a /home/${USER}/. /${USER}/
+rm -fr /home/${USER}
+echo "${USER}:${PASSWORD}" | chpasswd
+
+# use sudo or wheel
+GROUP=$(getent group sudo | cut -d':' -f1)
+GROUP=${GROUP:-$(getent group wheel | cut -d':' -f1)}
+
+# If the usermod succeeds then
+# create a sudoers.d file for the user.
+if usermod -aG ${GROUP} ${USER}; then
+  FILE="/etc/sudoers.d/${USER}"
+  echo "${USER} ALL=(ALL) NOPASSWD: ALL" > ${FILE}
+fi
+
