7a8834842ac5364769534f8f2b640f0d452ab69c
Owner <owner@ipa.css.cdn.local>
2021-01-17 18:47:23 -0500

    Switch 127.0.0.1 to CDN_IP in /etc/hosts template

--- bootstrap.d/02-resolv.sh
+++ bootstrap.d/02-resolv.sh
@@ -9,6 +9,11 @@ sed --in-place "s/#DNS=/DNS=10.236.0.23/" /etc/systemd/resolved.conf
 echo " Update Domains"
 sed --in-place "s/#Domains=/Domains=dss.cdn.local/" /etc/systemd/resolved.conf
 
-# This is for things that need the fqdn to resolve to 127.0.0.1
+# This is for things that need the fqdn to resolve to CDN_IP
 echo "Replace hosts template"
 cp /srv/ubuntu_config/etc/cloud/templates/hosts.debian.tmpl /etc/cloud/templates/hosts.debian.tmpl
+
+CDN_IP=$(ip route get 10.236.0.23 |  awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+
+sed --in-place "s/CDN_IP/${CDN_IP}" /etc/cloud/templates/hosts.debian.tmpl
+
