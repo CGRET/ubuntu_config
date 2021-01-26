992ee3b0693b3f8bd50f0b5800874234a76d042e
Owner <owner@ipa.css.cdn.local>
2021-01-26 18:02:48 -0500

    Working on slurm for nodes.

--- bootstrap.d/09-slurm.sh
+++ bootstrap.d/09-slurm.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 
-# Going to move these into freeIPA so they are 
-# stored in a central location
-#echo "Creating users and groups for slurm and munge."
-#adduser -u 1111 munge --disabled-password --gecos ""
-#adduser -u 1121 slurm --disabled-password --gecos ""
+logger -p user.info -t "bootstrap.${0}" "Install & configure munge and slurm"
 
+if [ ! -f /srv/.pass ]; then
+        logger -p user.info -t "bootstrap.${0}" "Missing password.  Will not install."
+        exit 0
+fi
+
+PASSWORD=$(cat /srv/.pass)
+
+logger -p user.info -t "bootstrap.${0}" "Creating users and groups for slurm and munge."
+adduser -u 1111 munge --disabled-password --gecos "" --no-create-home
+usermod --home /none munge
+adduser -u 1121 slurm --disabled-password --gecos "" --no-create-home
+usermod --home /none slurm
+
+logger -p user.info -t "bootstrap.${0}" "Installing munge"
+apt-get install --yes --quiet munge libmunge-dev libmunge2
+systemctl enable munge
+# need to get the munge.key from maas.dss.cdn.local/etc/munge/munge.key 
+#  to /etc/munge/munge.key
+#  will have to do this on first boot in rc.local
+
+
+logger -p user.info -t "bootstrap.${0}" "Installing slurm node"
+apt-get install --yes --quiet slurm-wlm slurm-wlm-basic-plugins slurm-wlm-torque slurmd
+systemctl enable slurmd
