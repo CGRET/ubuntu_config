70d5c25f4eac0e1319fa9b07c74117a65f5533bf
Owner <owner@ipa.css.cdn.local>
2021-01-29 11:29:03 -0500

    Updates

--- bootstrap.d/03-utilities.sh
+++ bootstrap.d/03-utilities.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#apt --yes install environment-modules
+#apt-get --yes --quiet install environment-modules
 
 echo "Installing utilities."
-apt-get --yes --quiet install ldap-utils ubuntu-drivers-common inetutils-traceroute sshpass mpich
+apt-get --yes --quiet install ldap-utils ubuntu-drivers-common inetutils-traceroute sshpass
 
 echo "Installing golang."
 apt-get --yes --quiet install golang
@@ -11,6 +11,12 @@ apt-get --yes --quiet install golang
 echo "Installing cockpit."
 apt-get --yes --quiet install cockpit cockpit-machines
 
+echo "Installing KVM/qemu add ons."
+apt-get --yes --quiet install qemu-system-ppc virt-top qemu-user-binfmt
 
-echo "Install KVM/qemu add ons"
-apt-get --yes --quet install qemu-system-ppc virt-top qemu-user-binfmt
+echo "Installing Open JDK 8."
+apt-get --yes --quiet install openjdk-8-jdk-headless
+echo 'JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/"' >> /etc/environment
+
+echo "Installing mpich."
+apt-get --yes --quiet install mpich
--- etc/rc.local.d/20-freeipa.sh
+++ etc/rc.local.d/20-freeipa.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
-# Configuration will be on first boot
+# Installation and configuration will be on first boot
+#  Note: need to do the install and config together w/o
+#        a reboot in between.  The install leaves the system
+#  	 in a poorly configured state.
 
-logger -p user.info -t rc.local  "Checking if we should configure freeIPA client."
+logger -p user.info -t "rc.local.${0}"  "Checking if we should configure freeIPA client."
 
 if [ -f /srv/.pass ]; then
 
-logger -p user.info -t rc.local "Credentials located."
+logger -p user.info -t "rc.local.${0}" "Credentials located."
 PASSWORD=$(cat /srv/.pass)
 
 target="10.236.0.23"
-logger -p user.info -t rc.local  "Checking for IP that can reach ${target}:"
+logger -p user.info -t "rc.local.${0}"  "Checking for IP that can reach ${target}:"
 count=$( ping -c 1 $target | grep icmp* | wc -l )
 if [ $count -eq 1 ]; then
 
 
 CDN_IP=$(ip route get ${target} |  awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
-logger -p user.info -t rc.local  "IP that can reach ${target}: ${CDN_IP}"
+logger -p user.info -t "rc.local.${0}"  "IP that can reach ${target}: ${CDN_IP}"
 
 # freeIPA requires fully-qualified hostname
-logger -p user.info -t rc.local  "Adding dss.cdn.local to hostname."
+logger -p user.info -t "rc.local.${0}"  "Adding dss.cdn.local to hostname."
 HOSTNAME=$(hostname -s).dss.cdn.local
-logger -p user.info -t rc.local  "hostname: ${HOSTNAME}"
+logger -p user.info -t "rc.local.${0}"  "hostname: ${HOSTNAME}"
 hostnamectl set-hostname ${HOSTNAME}
 
-logger -p user.info -t rc.local "Installing freeipa-client"
+logger -p user.info -t "rc.local.${0}" "Installing freeipa-client"
 DEBIAN_FRONTEND=noninteractive apt-get -yqq install freeipa-client
 
-logger -p user.info -t rc.local  "Configuring ${HOSTNAME} with ipa-client-install"
+logger -p user.info -t "rc.local.${0}"  "Configuring ${HOSTNAME} with ipa-client-install"
 ipa-client-install --unattended \
 --hostname=${HOSTNAME} \
 --mkhomedir \
@@ -47,58 +50,58 @@ ipa-client-install --unattended \
 OK=$?
 if [  "$OK" -eq "0" ]; then
 
-logger -p user.info -t rc.local  "Getting TGT from realm."
+logger -p user.info -t "rc.local.${0}"  "Getting TGT from realm."
 echo ${PASSWORD} | kinit admin
 
-logger -p user.info -t rc.local  "Adding A record and reverse PTR"
+logger -p user.info -t "rc.local.${0}"  "Adding A record and reverse PTR"
 # Note the . at the end of domain
 ipa dnsrecord-add dss.cdn.local. $(hostname -s) --a-rec=${CDN_IP} --a-create-reverse
 
 #ipa hostgroup-add massnodes --desc="MAAS Nodes"
 
-logger -p user.info -t rc.local  "Adding $(hostname) to massnodes hostgroup."
+logger -p user.info -t "rc.local.${0}"  "Adding $(hostname) to massnodes hostgroup."
 ipa hostgroup-add-member maasnodes --hosts $(hostname)
 
 # Create the clients key on the ipa server
-#logger -p user.info -t rc.local  "Creating client nfs/${HOSTNAME} entry."
+#logger -p user.info -t "rc.local.${0}"  "Creating client nfs/${HOSTNAME} entry."
 #ipa service-add nfs/${HOSTNAME}
 
 # Get the previously created key
 # and store it in the clients keytab
-#logger -p user.info -t rc.local  "Updating krb5.keytab with nfs/${HOSTNAME}"
+#logger -p user.info -t "rc.local.${0}"  "Updating krb5.keytab with nfs/${HOSTNAME}"
 #ipa-getkeytab \
 #--server=ipa.dss.cdn.local \
 #--principal=nfs/${HOSTNAME} \
 #--keytab=/etc/krb5.keytab
 
 # Create the clients key on the ipa server
-#logger -p user.info -t rc.local  "Creating client host/${HOSTNAME} entry."
+#logger -p user.info -t "rc.local.${0}"  "Creating client host/${HOSTNAME} entry."
 #ipa service-add host/${HOSTNAME}
 
 # Get the previously created key
 # and store it in the clients keytab
-#logger -p user.info -t rc.local  "Updating krb5.keytab with host/${HOSTNAME}"
+#logger -p user.info -t "rc.local.${0}"  "Updating krb5.keytab with host/${HOSTNAME}"
 #ipa-getkeytab \
 #--server=ipa.dss.cdn.local \
 #--principal=host/${HOSTNAME} \
 #--keytab=/etc/krb5.keytab
 
-logger -p user.info -t rc.local "Setting up automount."
+logger -p user.info -t "rc.local.${0}" "Setting up automount."
 ipa-client-automount \
 --unattended \
 --location=default \
 --server=ipa.dss.cdn.local
 
 rm /srv/.pass
-logger -p user.info -t rc.local "freeIPA configuration complete."
+logger -p user.info -t "rc.local.${0}" "freeIPA configuration complete."
 fi
 
 # end of ping check
 else
-	logger -p user.info -t rc.local  "Could not reach ${target}."
+	logger -p user.info -t "rc.local.${0}"  "Could not reach ${target}."
 fi
 
 # end of .pass check
 else
-	logger -p user.info -t rc.local  "Will not configure freeIPA: no password set."
+	logger -p user.info -t "rc.local.${0}"  "Will not configure freeIPA: no password set."
 fi
