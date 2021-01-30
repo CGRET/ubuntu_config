cdb96839a7552d1f50c6fcffa1c6a37332c808f0
Owner <owner@ipa.css.cdn.local>
2021-01-30 09:55:03 -0500

    Turn off cockpit, qemu, and mpich install

--- bootstrap.d/03-utilities.sh
+++ bootstrap.d/03-utilities.sh
@@ -8,11 +8,11 @@ apt-get --yes --quiet install ldap-utils ubuntu-drivers-common inetutils-tracero
 echo "Installing golang."
 apt-get --yes --quiet install golang
 
-echo "Installing cockpit."
-apt-get --yes --quiet install cockpit cockpit-machines
+#echo "Installing cockpit."
+#apt-get --yes --quiet install cockpit cockpit-machines
 
-echo "Installing KVM/qemu add ons."
-apt-get --yes --quiet install qemu-system-arm qemu-efi-aarch64 qemu-efi-arm qemu-system-ppc virt-top qemu-user-binfmt
+#echo "Installing KVM/qemu add ons."
+#apt-get --yes --quiet install qemu-system-arm qemu-efi-aarch64 qemu-efi-arm qemu-system-ppc virt-top qemu-user-binfmt
 
-echo "Installing mpich."
-apt-get --yes --quiet install mpich
+#echo "Installing mpich."
+#apt-get --yes --quiet install mpich
